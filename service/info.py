b77774d0a3103b00491ce259f27cd43cf7af0402
Piotr Pietruszewski <piotr.pietruszewski@consult.red>
2023-12-13 11:16:44 +0100

    Change "undefined" to "not specified" in the /info endpoint

--- service/info.py
+++ service/info.py
@@ -13,12 +13,12 @@ class Info:
         """Read info."""
         self.app_start_time = datetime.utcnow()
         self.host_name = socket.gethostname()
-        self.app_branch = os.getenv("APP_BRANCH", "undefined")
-        self.app_name = os.getenv("APP_NAME", "undefined")
-        self.app_build_time = os.getenv("APP_BUILD_TIME", "undefined")
-        self.app_version = os.getenv("APP_VERSION", "undefined")
-        self.stack_name = os.getenv("STACK_NAME", "undefined")
-        self.app_revision = os.getenv("APP_REVISION", "undefined")
+        self.app_branch = os.getenv("APP_BRANCH", "not specified")
+        self.app_name = os.getenv("APP_NAME", "not specified")
+        self.app_build_time = os.getenv("APP_BUILD_TIME", "not specified")
+        self.app_version = os.getenv("APP_VERSION", "not specified")
+        self.stack_name = os.getenv("STACK_NAME", "not specified")
+        self.app_revision = os.getenv("APP_REVISION", "not specified")
 
     def get(self) -> Dict[str, str]:
         """Return info."""
